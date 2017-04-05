1bea43843af29cd516ff96dc6c6835e372b69fab
MuslimBeibytuly <muslimbeibytuly@gmail.com>
2017-04-05 14:59:32 +0600

    jwt change

--- api/settings.py
+++ api/settings.py
@@ -141,5 +141,7 @@ REST_FRAMEWORK = {
 }
 
 JWT_AUTH = {
-    'JWT_REFRESH_EXPIRATION_DELTA': datetime.timedelta(days=7),
+    'JWT_EXPIRATION_DELTA': datetime.timedelta(days=30),
+    'JWT_ALLOW_REFRESH': True,
+    'JWT_REFRESH_EXPIRATION_DELTA': datetime.timedelta(days=365),
 }
