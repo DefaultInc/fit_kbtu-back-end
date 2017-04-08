b8a06d555cce0ec4083655a7f113781d1fe870f9
MuslimBeibytuly <muslimbeibytuly@gmail.com>
2017-04-08 16:58:06 +0600

    likes_urls

--- posts/urls.py
+++ posts/urls.py
@@ -1,8 +1,9 @@
 from django.conf.urls import url
-from .views import post_list, post_detail, comment_create
+from .views import post_list, post_detail, comment_create, like_post
 
 urlpatterns = [
     url(r'^posts/$', post_list),
     url(r'^posts/(?P<pk>[0-9]+)/$', post_detail),
     url(r'^comments/$', comment_create),
+    url(r'^like/$', like_post),
 ]
