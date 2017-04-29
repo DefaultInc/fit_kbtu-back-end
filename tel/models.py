820c7f8d7fccb6e2b2c3a6c52f416c3833ffecbc
MuslimBeibytuly <muslimbeibytuly@gmail.com>
2017-04-29 13:46:18 +0600

    docker_shit

--- tel/models.py
+++ tel/models.py
@@ -6,4 +6,4 @@ from django.db import models
 class Telephone(models.Model):
     user_name = models.CharField(max_length=256, null=False, blank=False, )
     phone = models.CharField(max_length=256, null=False, blank=False, )
-    created_date = models.DateTimeField(auto_now_add=True, blank=False, )
+    created_date = models.DateTimeField(auto_now_add=True, blank=False, null=False,)
--- tel/serializers.py
+++ tel/serializers.py
@@ -11,7 +11,10 @@ class TelephoneCreateSerializer(serializers.ModelSerializer):
     def create(self, validated_data):
         return Telephone.objects.create(**validated_data)
 
+
 class TelephoneSerializer(serializers.ModelSerializer):
+    created_date = serializers.DateTimeField()
+
     class Meta:
         model = Telephone
-        fields = ('user_name', 'phone',)
+        fields = ('user_name', 'phone', 'created_date',)
